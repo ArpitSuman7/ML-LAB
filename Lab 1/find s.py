f2d71d4a88710d9f73dd6214f03d7f96fe56429c
Arpit Suman <71319991+ArpitSuman7@users.noreply.github.com>
2022-06-09 00:24:44 +0530

    Update find s.py

--- Lab 1/find s.py
+++ Lab 1/find s.py
@@ -1,40 +1,29 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import pandas as pd
 import numpy as np
+ 
+data = pd.read_csv(r"datalab.csv")
 
-
-# In[8]:
-
-
-
-data = pd.read_csv("data.csv")
-print(data,"\n")
+ 
 d = np.array(data)[:,:-1]
-print("Attributes : ",d)
+print("\n The attributes are: ",d)
+ 
 target = np.array(data)[:,-1]
-print("\n Target : ",target)
-def findS(c,t):
-    for i,val in enumerate(t):
-        if val=="yes":
-            sh = c[i].copy()
+print("\n The target is: ",target)
+ 
+def train(c,t):
+    for i, val in enumerate(t):
+        if val == "Yes":
+            specific_hypothesis = c[i].copy()
             break
-    for i,val in enumerate(c):
-        if t[i]=="yes":
-            for x in range(len(sh)):
-                if val[x] != sh[x]:
-                    sh[x] = '?'
+             
+    for i, val in enumerate(c):
+        if t[i] == "Yes":
+            for x in range(len(specific_hypothesis)):
+                if val[x] != specific_hypothesis[x]:
+                    specific_hypothesis[x] = '?'
                 else:
                     pass
-    return sh
-print("Final hypothesis: ",findS(d,target))
+                 
+    return specific_hypothesis
 
-
-# In[ ]:
-
-
-
+print("\n The final hypothesis is:",train(d,target))
